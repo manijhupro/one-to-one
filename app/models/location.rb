a1ecef760d7a6299f6dc52b09b5be876418ce197
sushilthe <sushilthe@gmail.com>
2014-04-26 14:40:19 +0545

    Location added

---
+++ app/models/location.rb
@@ -0,0 +1,6 @@
+class Location < ActiveRecord::Base
+  attr_accessible :address, :latitude, :longitude
+  validates :address, presence:true
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
+end
