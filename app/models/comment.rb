8aff2a9eb9b962fbb3b885e2d60ea3c9a29a86e2
mani <mani.jhupro@gmail.com>
2014-07-13 17:25:02 +0545

    changes in services

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   attr_accessible :body, :email
+  validates_presence_of :body, :email
+  #
+  #validates :body, presence: true, length: { maximum: 50 }
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #validates :email, presence:   true,
+  #          format:     { with: VALID_EMAIL_REGEX }
+  #
+  #
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,5 @@ class Post < ActiveRecord::Base
   attr_accessible :body, :title
 
   has_many :comments
-
   validates_presence_of :body, :title
 end
